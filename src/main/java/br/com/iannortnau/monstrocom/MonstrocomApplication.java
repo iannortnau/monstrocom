f170984333e57f96bda701bdce4d6401da2cadad
Iann Ortnau <iann.santos@e-unicred.com.br>
2022-11-02 19:47:06 -0300

    primeiro commit

---
+++ src/main/java/br/com/iannortnau/monstrocom/MonstrocomApplication.java
@@ -0,0 +1,13 @@
+package br.com.iannortnau.monstrocom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonstrocomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonstrocomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/iannortnau/monstrocom/MonstrocomApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iannortnau.monstrocom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonstrocomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
